d05704e5dfc4283dd674077ee2550b176a8369b7
osc <ohbravo7@gmail.com>
2020-02-27 15:42:35 +0900

    .gitignore

---
+++ src/main/java/com/spring1/spring2/spring3/web/dto/HelloResponseDto.java
@@ -0,0 +1,11 @@
+package com.spring1.spring2.spring3.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
