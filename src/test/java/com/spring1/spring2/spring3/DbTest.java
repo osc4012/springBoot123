0287fef23749e457ee8b50ee21c959c8570683db
osc <ohbravo7@gmail.com>
2020-03-16 16:20:35 +0900

    340

---
+++ src/test/java/com/spring1/spring2/spring3/DbTest.java
@@ -0,0 +1,26 @@
+package com.spring1.spring2.spring3;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbTest {
+    private static final String DRIVER = "org.mariadb.jdbc.Driver";
+    private static final String URL = "jdbc:mariadb://freelec-springboot2-webservice2.cuxnxrgehovo.ap-northeast-2.rds.amazonaws.com:3306/freelec_springboot2_webservice2"; // jdbc:mysql://127.0.0.1:3306/여러분이 만드신 스키마이름
+    private static final String USER = "osc"; //DB 사용자명
+    private static final String PW = "1q2w3e4r";   //DB 사용자 비밀번호
+
+    @Test
+    public void testConnection() throws Exception{
+        Class.forName(DRIVER);
+
+        try(Connection con = DriverManager.getConnection(URL, USER, PW)){
+            System.out.println("성공");
+            System.out.println(con);
+        }catch (Exception e) {
+            System.out.println("에러발생");
+            e.printStackTrace();
+        }
+    }
+}
